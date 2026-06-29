1462fda093300b7a7f6157c6ed1dcc1f602bf30e
TerserahGw <120995106+TerserahGw@users.noreply.github.com>
2026-06-29 19:38:14 +0700

    Update constants.ts

--- src/configs/constants.ts
+++ src/configs/constants.ts
@@ -1,10 +1,10 @@
-export const PLUGIN_ID = 'hallofcodes.rutex.coding_agent'
-export const AI_SETTINGS_STORAGE_KEY = 'rutex_settings'
-export const CHAT_HISTORY_KEYS = 'rutex_chat_history_keys'
-export const LAST_ACTIVE_CHAT_HISTORY_KEY = 'rutex_chat_history_last_active'
+export const PLUGIN_ID = 'chioriya.conduit.coding_agent'
+export const AI_SETTINGS_STORAGE_KEY = 'conduit_agent_settings'
+export const CHAT_HISTORY_KEYS = 'conduit_agent_chat_history_keys'
+export const LAST_ACTIVE_CHAT_HISTORY_KEY = 'conduit_agent_chat_history_last_active'
 
 export const RUTEX_DB_VERSION = 1
-export const RUTEX_DB_NAME = 'rutex_chat_history'
+export const RUTEX_DB_NAME = 'conduit_agent_chat_history'
 
 export const CHAT_HISTORY_STORE = 'chat_histories'
 export const EDITED_FILE_HISTORY_STORE = 'edited_histories'
